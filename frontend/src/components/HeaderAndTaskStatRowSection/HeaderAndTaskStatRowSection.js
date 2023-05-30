7d54b6df5e4419f44f4ed37d509cef9817228114
unknown <nguyensongvo2652001@gmail.com>
2023-05-30 10:55:29 +0700

    refractor some code

---
+++ frontend/src/components/HeaderAndTaskStatRowSection/HeaderAndTaskStatRowSection.js
@@ -0,0 +1,59 @@
+import Dropdown from "../UI/Dropdown/Dropdown";
+import ConstantContext from "../../contexts/ConstantContext.js";
+
+import { useContext } from "react";
+
+import { capitalizeFirstLetter } from "../../utils/string";
+
+import classes from "./HeaderAndTaskStatRowSection.module.css";
+
+const HeaderAndTaskStatRowSection = (props) => {
+  const { project, tasksCountBasedOnStatus } = props;
+  const constantContext = useContext(ConstantContext);
+
+  const taskStatusOptions = constantContext.TASK_STATUS_CONSTANT;
+  const statusDropdownOptions = [
+    "All",
+    ...taskStatusOptions.map((status) => capitalizeFirstLetter(status)),
+  ];
+
+  let displayProjectName = project.name;
+  if (displayProjectName.length > 30) {
+    displayProjectName = displayProjectName.slice(0, 27) + "...";
+  }
+
+  return (
+    <>
+      <header className={classes.header}>
+        <h1 className={classes.header__projectName}>{displayProjectName}</h1>
+        <div className={classes.header__buttonAndDropdown}>
+          <button className={classes.header__newTaskButton}>New Task</button>
+          <Dropdown
+            className={classes.header__statusDropdown}
+            options={statusDropdownOptions}
+          />
+        </div>
+      </header>
+
+      <ul className={classes.taskStatRow}>
+        {taskStatusOptions.map((option, index) => {
+          let allClasses = `${classes.taskStatBox} `;
+          const classNameBasedOnStatus = `taskStatBox--${option}`;
+          allClasses += classes[classNameBasedOnStatus];
+          return (
+            <li key={index} className={allClasses}>
+              <p className={classes.taskStatValue}>
+                {tasksCountBasedOnStatus[option]}
+              </p>
+              <p className={classes.taskStatLabel}>
+                {capitalizeFirstLetter(option)} Tasks
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default HeaderAndTaskStatRowSection;
--- frontend/src/components/ProjectDashboard/ProjectDashboard.js
+++ frontend/src/components/ProjectDashboard/ProjectDashboard.js
@@ -12,16 +12,12 @@ import classes from "./ProjectDashboard.module.css";
 import { capitalizeFirstLetter } from "../../utils/string";
 import TaskCard from "../UI/TaskCard/TaskCard.js";
 import Task from "../Task/Task.js";
+import HeaderAndTaskStatRowSection from "../HeaderAndTaskStatRowSection/HeaderAndTaskStatRowSection.js";
 
 const ProjectDashboard = (props) => {
-  const constantContext = useContext(ConstantContext);
-
-  const { id: projectId } = useParams();
-  let projectName = "Twitter Clone ";
-
-  if (projectName.length > 30) {
-    projectName = projectName.slice(0, 27) + "...";
-  }
+  const project = {
+    name: "Twitter Clone",
+  };
 
   const [tasksCountBasedOnStatus, setTasksCountBasedOnStatus] = useState({
     open: 48,
@@ -30,12 +26,6 @@ const ProjectDashboard = (props) => {
     testing: 0,
     overdue: 52,
   });
-
-  const taskStatusOptions = constantContext.TASK_STATUS_CONSTANT;
-  const statusDropdownOptions = [
-    "All",
-    ...taskStatusOptions.map((status) => capitalizeFirstLetter(status)),
-  ];
 
   const taskDisplayMetaInfo = [
     {
@@ -50,36 +40,10 @@ const ProjectDashboard = (props) => {
 
   return (
     <div className={classes.projectDashboard}>
-      <header className={classes.projectDashboard__header}>
-        <h1 className={classes.projectDashboard__projectName}>{projectName}</h1>
-        <div className={classes.projectDashboard__header__buttonAndDropdown}>
-          <button className={classes.projectDashboard__newTaskButton}>
-            New Task
-          </button>
-          <Dropdown
-            className={classes.projectDashboard__statusDropdown}
-            options={statusDropdownOptions}
-          />
-        </div>
-      </header>
-
-      <ul className={classes.projectDashboard__taskStatRow}>
-        {taskStatusOptions.map((option, index) => {
-          let allClasses = `${classes.projectDashboard__taskStatBox} `;
-          const classNameBasedOnStatus = `projectDashboard__taskStatBox--${option}`;
-          allClasses += classes[classNameBasedOnStatus];
-          return (
-            <li key={index} className={allClasses}>
-              <p className={classes.projectDashboard__taskStatValue}>
-                {tasksCountBasedOnStatus[option]}
-              </p>
-              <p className={classes.projectDashboard__taskStatLabel}>
-                {capitalizeFirstLetter(option)} Tasks
-              </p>
-            </li>
-          );
-        })}
-      </ul>
+      <HeaderAndTaskStatRowSection
+        project={project}
+        tasksCountBasedOnStatus={tasksCountBasedOnStatus}
+      />
 
       <ul className={classes.projectDashboard__tasks}>
         <Task
